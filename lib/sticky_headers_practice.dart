1558691bbad9b84acbd4aa117c54de5bba8e7f2c
sonsy <sonsy@smsoftlab.co.kr>
2024-02-26 09:36:27 +0900

    프로젝트 생성

---
+++ lib/sticky_headers_practice.dart
@@ -0,0 +1,7 @@
+library sticky_headers_practice;
+
+/// A Calculator.
+class Sticky {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
---
+++ test/sticky_headers_practice_test.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:sticky_headers_practice/sticky_headers_practice.dart';
+
+void main() {
+  test('adds one to input values', () {
+    final calculator = Calculator();
+    expect(calculator.addOne(2), 3);
+    expect(calculator.addOne(-7), -6);
+    expect(calculator.addOne(0), 1);
+  });
+}
